ee0af95dd2005b53b82230c016a2eff86e8b1c4c
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-10-01 18:34:35 -0400

    commit tree 90dcf6a027093956e5917308bf9185eb3a760ceb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator array_names
+    array_names.map { |name| "Hello, my name is #{name}."}
+end
+
+
+def assign_rooms speakers_array
+   speakers_array.map.with_index(1) {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index}!"}
+end
+
+def printer array
+   batch_badge_creator(array).map {|x| puts x}
+   assign_rooms(array).map {|x| puts x}
+end
